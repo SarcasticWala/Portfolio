97b64490f341007d027343e27ad22334a2668c33
SarcasticWala <dasayan948@gmail.com>
2025-04-27 03:32:11 +0530

    image upload

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,8 +1,12 @@
+import pic from "../assets/image1.png";
+import pic2 from  "../assets/image2.png";
+import pic3 from  "../assets/imag3.png";
+import pic4 from  "../assets/image.png";
 export const projects = [
   {
     title: "Chat-A-Verse",
     description: "A full-featured chatting platform with Built and launched a scalable and secure web chat app with MERN stack and socket.io, enabling one-on-one and groupchat, search, and a new notification system that boosted user engagement by 20%. .",
-    image: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?auto=format&fit=crop&q=80&w=1000",
+    image: pic2,
     technologies: ["React.js", "MongoDB", "Express.js", "Node.js", "Socket.io"],
     githubUrl: "https://github.com/SarcasticWala/Chat_Application",
     liveUrl: "https://chat-a-verse-backend.onrender.com/"
@@ -10,10 +14,26 @@ export const projects = [
   {
     title: "CodeSphere",
     description: "A CodeSphere, a real-time collaborative coding platform with features like live code compilation, screensharing, chat, and room management, supporting multiple programming languages, Implemented room-based coding environments, enabling real-time collaboration and feedback for users in differentlanguages like C, C++, Java, Python, and more.",
-    image: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?auto=format&fit=crop&q=80&w=1000",
+    image: pic3,
     technologies: ["React", "Node.js", "Socket.io", "MongoDB", "Monaco Editor"],
     githubUrl: "https://github.com/SarcasticWala/Code_Sphere",
     liveUrl: "https://code-sphere-editor.vercel.app/"
+  },
+  {
+    title: "Esperenza 2k25",
+    description: "An expense tracker app that allows users to track their income and expenses, visualize thePlayed a key role in developing the offi cial website for Esperanza 2k25, the tech-cum-cultural fest of Coochbehar Government Engineering College. Contributed major features and commits as part of a collaborative team eff ort",
+    image: pic,
+    technologies: ["Next.js", "TypeScript", "MongoDB", "Tailwind CSS", "Acertinity UI", "Shadcn UI"],
+    githubUrl: "https://github.com/SarcasticWala/Esperanza_2k25",
+    liveUrl: "https://www.esperanza.org.in/"
+  },
+  {
+    title: "Your_Games",
+    description: "A platform for gamers to connect, share, and discover new games. Users can create profiles, post reviews, and join gaming communities.",
+    image: pic4,
+    technologies: ["React","TypeScript", "MongoDB","Express.js","Node.js", "Tailwind CSS"],
+    githubUrl: "https://github.com/SarcasticWala/game",
+    liveUrl: "https://game-mmo7.vercel.app/"
   },
   {
     title: "expense-tracker",
@@ -23,12 +43,5 @@ export const projects = [
     githubUrl: "https://github.com/SarcasticWala/Expense_Tracker",
     liveUrl: "https://expense-tracker-uql7.onrender.com/"
   },
-  // {
-  //   title: "Social Media Platform",
-  //   description: "A modern social networking platform with features like real-time messaging, content sharing, and personalized feed algorithms.",
-  //   image: "https://images.unsplash.com/photo-1611162617474-5b21e879e113?auto=format&fit=crop&q=80&w=1000",
-  //   technologies: ["React Native", "GraphQL", "AWS", "Firebase", "Node.js"],
-  //   githubUrl: "https://github.com/yourusername/social-platform",
-  //   liveUrl: "https://social-platform.example.com"
-  // }
+ 
 ];
